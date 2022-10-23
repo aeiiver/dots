67bb42aa3a6c1d08137b86b1d75ad036c2a26bd6
aeiiver <101661283+aeiiver@users.noreply.github.com>
2022-10-23 14:07:59 +0200

    Restore NF icons

--- nvim/.config/nvim/lua/nahida/packer.lua
+++ nvim/.config/nvim/lua/nahida/packer.lua
@@ -31,7 +31,7 @@ return require('packer').startup(function(use)
     }
     use {
         'nvim-lualine/lualine.nvim',
-        --requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
     }
     use { 'jacoborus/tender.vim' }
 end)

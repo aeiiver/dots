fb33bac62a93f9559099088f008dee3d3cf21638
aeiiver <101661283+aeiiver@users.noreply.github.com>
2022-10-23 16:56:26 +0200

    Move shell cfg into a stow-ignore shared dir

--- .share/pwsh/.pwsh.ps1
+++ .share/pwsh/.pwsh.ps1
@@ -1,5 +1,5 @@
 # Add the following line in your $PROFILE
-# . "${env:USERPROFILE}\dots\pwsh\.pwsh.ps1"
+# . "${env:USERPROFILE}\dots\.share\pwsh\.pwsh.ps1"
 
 $this_dir = (Split-Path $MyInvocation.MyCommand.Path)
 if (Test-Path -Path "${this_dir}\.pwsh.d") {
